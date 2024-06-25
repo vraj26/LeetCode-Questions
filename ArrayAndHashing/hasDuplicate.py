ff4fb33e2c4d5a1ce6ada9eee1d0539413e74296
Vraj Patel <72108286+vraj26@users.noreply.github.com>
2024-06-25 14:12:25 -0400

    Update hasDuplicate.py

--- ArrayAndHashing/hasDuplicate.py
+++ ArrayAndHashing/hasDuplicate.py
@@ -1,5 +1,7 @@
 class Solution:
+    #Time: O(n^2), Space: O(1)
     def hasDuplicate(self, nums: List[int]) -> bool:
+        #check each element with another and see if it matches
          for n in range(len(nums)):
             for j in range(n+1, len(nums)):
                 if nums[n] == nums[j]:
@@ -16,7 +18,7 @@ class Solution:
          for i in range(len(nums)):
             hashmap[nums[i]] = 1 + hashmap.get(nums[i], 0)
 
-        #loop through the hashmap and see if there is a numver occurring more than once
+        #loop through the hashmap and see if there is a number occurring more than once
          for n in hashmap:
             if hashmap[n] > 1:
                 return True
