2d896357c9dd8d770147d763b0b9a97080784efb
Vraj Patel <72108286+vraj26@users.noreply.github.com>
2024-06-30 10:19:11 -0400

    Create AnagramGroups.py

---
+++ ArrayAndHashing/AnagramGroups.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list) #hashmap initialized such that there are no edge cases
+
+        #loop through the strs list as s being individual strings
+        for s in strs:
+            count = [0] * 26 #init empty array of 26 0s, each representing count of alphabet
+            
+            #loop through the strings, c being individual letter
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+
+            #the key being the count of letters and values being anagrams
+            result[tuple(count)].append(s)
+
+        return result.values()
+
