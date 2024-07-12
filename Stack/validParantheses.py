c8d1176669127791fcb39c9539e8694c36288915
Vraj Patel <72108286+vraj26@users.noreply.github.com>
2024-07-12 08:55:51 -0400

    Create validParantheses.py

---
+++ Stack/validParantheses.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        #init stack and map each closing bracket to opening
+        stack = []
+        closeToOpen = {")" : "(",
+                       "]" : "[",
+                       "}" : "{"}
+
+        #loop through input
+        for bracket in s:
+
+            #if the bracket is a closing bracket then...
+            if bracket in closeToOpen:
+
+                #if the stack is not empty and the value at top of stack is the same as the matching open parantheses..
+                if stack and stack[-1] == closeToOpen[bracket]:
+
+                    #pop from the stack, else the string is invalid
+                    stack.pop()
+                else:
+                    return False
+            
+            #if the bracket is opening bracket then simply append
+            else:
+                stack.append(bracket)
+        
+        #If the stack is empty return true, otherwise return false
+        return True if not stack else False
