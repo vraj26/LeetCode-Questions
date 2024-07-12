4f9e46bf37707a8871473fa5c44078da68e6c06d
Vraj Patel <72108286+vraj26@users.noreply.github.com>
2024-07-12 08:03:13 -0400

    Create productExceptSelf.py

---
+++ ArrayAndHashing/productExceptSelf.py
@@ -0,0 +1,23 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        #init output array of 1s w/ same length as nums
+        result = [1] * len(nums)
+
+        #init prefix value and loop through nums and at each position i, set that to prefix
+        prefix = 1
+        for i in range(len(nums)):
+            result[i] = prefix
+
+            #update prefix value
+            prefix *= nums[i]
+
+        #postfix pass
+        postfix = 1
+        for i in range(len(nums) -1, -1, -1):
+
+            #multiplies prefix with postfix
+            result[i] *= postfix
+            postfix *= nums[i]
+        
+        return result
